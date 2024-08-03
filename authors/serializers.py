0e9a74756af9d7cc68be8c09b3bf6df84da1e79c
rizo <rizoubaydullayev17@gmail.com>
2024-08-04 01:02:42 +0500

    Lesson_1_done

---
+++ authors/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework.serializers import ModelSerializer 
+
+
+from .models import Author
+
+class AuthorModelSerializer(ModelSerializer):
+
+
+    class Meta:
+        model = Author
+        fields = '__all__'
+        # fields = '__all__'
